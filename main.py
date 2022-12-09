72c2f2b7a24f5b07b0101fc50c433940857e3ab1
uedNguyenHuuPhuoc <pn0165652@gmail.com>
2022-12-09 20:59:33 +0700

    version 1.1 -- updated CLI interface

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from gazpacho import get, Soup
 from bs4 import BeautifulSoup
-from pathlib import Path
+from tqdm import tqdm
+from colorama import init
+
 import requests
 import pypandoc
+init()
 
-novel_url = "https://truyenyy.vip/truyen/dinh-cap-khi-van-lang-le-tu-luyen-ngan-nam/"
-resp_html = get(novel_url)
-html = Soup(resp_html)
-
-end_chapter_url = html.find("a", {'class': 'flex-primary'}, partial=False, mode='first').attrs["href"]
-total_chapter = int(end_chapter_url[end_chapter_url.rfind("-")+1:].split('.html')[0])
-prefix_url = "https://truyenyy.vip/truyen/"
-novel_name = end_chapter_url.split("/")[-2]
-novel_name_url = novel_name + '/'
-
-def get_title_html(soup):
+def get_title(soup):
     return soup.find('h2', {'class': 'heading-font mt-2'})
 
 def get_content_html(url, chapter):
     resp = get(url)
     bf_content_html = BeautifulSoup(resp, features="lxml").find("div", {"id": "inner_chap_content_1"})
-    bf_title_html = get_title_html(BeautifulSoup(resp, features="lxml")).getText()
+    bf_title_html = get_title(BeautifulSoup(resp, features="lxml")).getText()
     chapter_title = f'Chương {chapter}: {bf_title_html}'
     return (chapter_title, str(bf_content_html))
 
@@ -43,15 +36,33 @@ def get_chapter_title(url):
     soup = BeautifulSoup(resp, features="lxml")
     return soup.find("h1", {"class": "name"}).getText()
 
-chapter_file_list = []
-(title, author, description, cover_img_path) = get_metadata(novel_url)
+def get_epub(novel_url):
+    resp_html = get(novel_url)
+    html = Soup(resp_html)
 
-for chapter in range(1, 15):
-    chapter_url = prefix_url + novel_name_url + f'chuong-{chapter}.html'
-    (chapter_title, chapter_content) = get_content_html(chapter_url, chapter)
-    pypandoc.convert_text(chapter_content, 'epub', 'html', extra_args=[f'--metadata=title:{chapter_title}'], outputfile=f"./output/{str(chapter).zfill(6)}.epub")
-    chapter_file_list.insert(0, f"./output/{str(chapter).zfill(6)}.epub")
+    end_chapter_url = html.find("a", {'class': 'flex-primary'}, partial=False, mode='first').attrs["href"]
+    # total_chapter = int(end_chapter_url[end_chapter_url.rfind("-")+1:].split('.html')[0])
+    total_chapter = 5
+    prefix_url = "https://truyenyy.vip/truyen/"
+    novel_name = end_chapter_url.split("/")[-2]
+    novel_name_url = novel_name + '/'
 
-print(chapter_file_list)
+    chapter_file_list = []
+    (title, author, description, cover_img_path) = get_metadata(novel_url)
+    print(f"Starting... \n\033[95mTotal chapter: {total_chapter}")
 
-pypandoc.convert_file(chapter_file_list, 'epub', 'epub', extra_args=[f'--metadata=title:{title}', f'--metadata=author:{author}', f'--metadata=description:{description}'], outputfile=f"{novel_name}.epub")
+    pbar = tqdm( unit="chapter", total=total_chapter, ascii=True, desc=f'\033[93mProgress', colour="green" )
+
+    for chapter in range(1, total_chapter + 1):
+        chapter_url = prefix_url + novel_name_url + f'chuong-{chapter}.html'
+        (chapter_title, chapter_content) = get_content_html(chapter_url, chapter)
+        pypandoc.convert_text(chapter_content, 'epub', 'html', extra_args=[f'--metadata=title:{chapter_title}'], outputfile=f"./output/{str(chapter).zfill(6)}.epub")
+        chapter_file_list.insert(0, f"./output/{str(chapter).zfill(6)}.epub")
+        pbar.update(1)
+
+    pypandoc.convert_file(chapter_file_list, 'epub', 'epub', extra_args=[f'--metadata=title:{title}', f'--metadata=author:{author}', f'--metadata=description:{description}'], outputfile=f"{novel_name}.epub")
+
+if(__name__ == "__main__"):
+    novel_url = "https://truyenyy.vip/truyen/dinh-cap-khi-van-lang-le-tu-luyen-ngan-nam/"
+    get_epub(novel_url)
+    print("\033[92mAll completed!")
